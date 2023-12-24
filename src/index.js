b834cba09c88192da984708ed47f1e8141447d74
oshkap <alexandra_shkap@ukr.net>
2023-12-24 15:44:07 +0100

    icon element added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ function refreshWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-tempetarature-icon" />`;
+
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
